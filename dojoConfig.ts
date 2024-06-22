e3fee2d38746f74bac4a27634e70c77ddf939750
Florian Charlier <98014814+trevis-dev@users.noreply.github.com>
2024-06-22 13:29:35 +0200

    initial commit: dojo react starter code

---
+++ dojoConfig.ts
@@ -0,0 +1,7 @@
+import manifest from "../dojo-starter/manifests/dev/manifest.json";
+import { createDojoConfig } from "@dojoengine/core";
+
+export const dojoConfig = createDojoConfig({
+    manifest,
+    toriiUrl: "http://localhost:8080"
+});
